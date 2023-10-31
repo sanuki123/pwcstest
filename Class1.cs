3adad018bc714096aae81033786a7d510a66503d
sanuki123 <gmbeth@42nd.sakura.ne.jp>
2023-10-31 13:44:26 +0900

    a

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaywrightTests
+{
+    public class Class1
+    {
+
+        public void aaa()
+        {
+            int a = 0;
+
+            if(a==1)
+            {
+                Console.WriteLine("aaaa");
+            }
+        }
+
+    }
+}
